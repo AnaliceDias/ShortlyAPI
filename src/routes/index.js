1f035bc3527a27a2f59acf1593f9c404c28b06da
Analice <anadias.alice@gmail.com>
2022-06-10 10:32:21 -0300

    feat: criação das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
+import rankingRouter from "./ranking/index.js";
+import signinRouter from "./signin/index.js";
+import signupRouter from "./signup/index.js";
+import urlsRouter from "./urls/index.js";
+import usersRouter from "./users/index.js";
 
 const router = Router();
+
+router.use(signinRouter);
+router.use(signupRouter);
+router.use(urlsRouter);
+router.use(usersRouter);
+router.use(rankingRouter);
 
 export default router;
---
+++ src/routes/signin/index.js
@@ -0,0 +1,7 @@
+import {Router} from "express";
+
+const signinRouter = Router();
+
+signinRouter.post("/signin");
+
+export default signinRouter;
