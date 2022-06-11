e15ffd6068b4f7ba19a130f9c87cfcb911ccc041
Analice <anadias.alice@gmail.com>
2022-06-10 23:04:14 -0300

    Prepare for deploy 4

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,11 +1,23 @@
 import db from "../../db/index.js";
 
+
+
 export async function listarURLs(req , res){
     
     try{
+        const totalCliques = await somarCliques();
+
         const solicitarLista = await db.query(`
-        SELECT * FROM usuarios
-        WHERE id = $1 AND token = $2
+        SELECT 
+            "urlsEncurtadas".id, 
+            usuarios.nome ,
+            "urlsEncurtadas"."urlEncurtada" as "shortUrl", 
+            "urlsEncurtadas"."urlOriginal" as "url", 
+            "urlsEncurtadas"."numeroCliques" as "visitCount"
+        FROM "urlsEncurtadas" 
+        JOIN usuarios
+        ON usuarios.id =  "usuarioId"
+        WHERE usuarios.id = $1 AND token = $2
         ` , [req.params.id , res.locals.token]);
 
         if(solicitarLista.rows.length === 0){
@@ -13,16 +25,27 @@ export async function listarURLs(req , res){
         }else{
             let resposta = {
                 id: solicitarLista.rows[0].id,
-                name: solicitarLista.rows[0].nome
+                name: solicitarLista.rows[0].nome,
+                visitCount: totalCliques,
+                shortenedUrls: [...solicitarLista.rows]
             }
 
-            
-
-            res.sendStatus(200);
+            res.status(200).send(resposta);
         }
 
     }catch(err){
         res.sendStatus(404);
+    }
+
+    async function somarCliques(){
+        const totalCliques = await db.query(`
+            SELECT SUM("urlsEncurtadas"."numeroCliques") 
+            FROM usuarios 
+            JOIN "urlsEncurtadas"
+            ON usuarios.id =  "usuarioId"
+        `);
+
+        return totalCliques.rows[0].sum;
     }
     
 }
